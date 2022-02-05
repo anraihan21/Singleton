2d19b00ddf89162fa48e18548d1e9a73908803e3
Shah Abdullah Al Nowman <nowman.softifybd@gmail.com>
2022-02-05 10:40:15 +0600

    Singleton structure first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                ()=> PrintFromEmployee(),
+                ()=> PrinFromStudent()
+                );
+
+            Console.ReadLine();
+        }
+
+        private static void PrinFromStudent()
+        {
+            Singleton fromStd = Singleton.GetSingleton;
+            fromStd.PrintIt("msg2");
+        }
+
+        private static void PrintFromEmployee()
+        {
+            Singleton fromEmployee = Singleton.GetSingleton;
+            fromEmployee.PrintIt("msg1");
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static int cnt = 0;
+        public static readonly object obj = new object();
+        private static readonly Lazy<Singleton> abc = new Lazy<Singleton>(()=> new Singleton());
+        public static Singleton GetSingleton
+        {
+            get
+            {
+                return abc.Value;
+            }
+        }
+        private Singleton()
+        {
+            cnt++;
+            Console.WriteLine("Count: " + cnt);
+        }
+        public void PrintIt(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
